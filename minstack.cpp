4f51b70a5734cb41054f37b96aa5d39b7def9a70
gaurav-foroll <gm890205@gmail.com>
2024-06-19 09:18:38 +0530

    min stack , rev polish , valid parentheses

---
+++ minstack.cpp
@@ -0,0 +1,34 @@
+class MinStack
+{
+public:
+    std::stack<std::pair<int, int>> st;
+
+    MinStack() {}
+
+    void push(int val)
+    {
+        if (!st.empty())
+        {
+            st.push({val, std::min(st.top().second, val)});
+        }
+        else
+        {
+            st.push({val, val});
+        }
+    }
+
+    void pop() { st.pop(); }
+
+    int top() { return st.top().first; }
+
+    int getMin() { return st.top().second; }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
